e815976ee1a07dd52a0ec8b5970c0c689e543842
María A. Rodríguez <72228855+MariaRod123@users.noreply.github.com>
2023-07-28 16:56:23 -0300

    Update decodificador.py

--- decodificador.py
+++ decodificador.py
@@ -1,6 +1,6 @@
 import base64
 
-mensaje_codificado = "VGUgZmVsaWNpdG8sIHN1cGVyYXN0ZSBlc3RhIHBydWViYS4gVGUgcGlkbyBxdWUgdG9tZXMgY2FwdHVyYSBkZSBwYW50YWxsYSBkZSBlc3RvIGp1bnRvIGNvbiBsYSBmZWNoYSB5IGhvcmEgeSBub3MgZW52w61lcyBlc3RhIGNhcHR1cmEgZW4gdW4gZW1haWwganVudG8gY29uIGVsIHJlc3RvIGRlIGxvcyBkZXNhZsOtb3MuIFBvciBmYXZvciBleHBsaWNhbm9zIGVuIGVsIG1haWwgY29tbyByZXNvbHZpc3RlIGVsIGFjZXJ0aWpvLg=="
+mensaje_codificado = input("Ingrese el código a descifrar: ")
 
 try:
   mensaje_decodificado=base64.b64decode(mensaje_codificado).decode('utf-8') #Intenta decodificar el mensaje si está en formato base64  
