752f693604e666954fb68c615ff4759ca8a914d3
María A. Rodríguez <72228855+MariaRod123@users.noreply.github.com>
2023-08-01 18:27:54 -0300

    arreglado el problema de la exception

--- decodificador.py
+++ decodificador.py
@@ -1,11 +1,12 @@
 import base64
+import binascii
 
 mensaje_codificado = input("Ingrese el código a descifrar: ")
 
 try:
   mensaje_decodificado=base64.b64decode(mensaje_codificado).decode('utf-8') #Intenta decodificar el mensaje si está en formato base64  
   print(mensaje_decodificado)
-except:
+except binascii.Error:
   print("Lo siento, no pude descifrar el mensaje porque está codificado en un formato diferente") #Si no está en formato base64 manda este mensaje
 
 
