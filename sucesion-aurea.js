e5cf8bf104b2ce97a838df5ca09176f353f06a55
María A. Rodríguez <72228855+MariaRod123@users.noreply.github.com>
2023-08-02 14:30:53 -0300

    arreglé con parseFloat porque los valores nuevos mostraban comillas

--- sucesion-aurea.js
+++ sucesion-aurea.js
@@ -1,18 +1,20 @@
 /*Resolución del ejercicio 3 : el patrón que siguen los números del array tiende a ser el producto del número áureo por el número anterior*/
 
 let array_inicial=[1, 1.618, 2.618, 4.236];
-console.log('Numeros iniciales:' + array_inicial);
+console.log('Numeros iniciales:', array_inicial);
 
 /* en la letra del ejercicio se nombra " paso dorado" haciendo referencia al número aúreo= phi"*/
 
 const phi= (1+ Math.sqrt(5))/2;  /*phi está definido matemáticamente a través esta expresión*/
 
 
-let cantNumeros_siguientes= 10; /* Cntidad de números que quiero ver despues de los iniciales */
+let cantNumeros_siguientes= 5; /* Cntidad de números que quiero ver despues de los iniciales */
 
 for (let i=0; i < cantNumeros_siguientes; i++){
     let elemento_siguiente= array_inicial[array_inicial.length -1] * phi; /*multiplica el valor anterior x el aúreo para obtener el siguiente en el array*/ 
-    array_inicial.push(elemento_siguiente); /* push() es un método de los array que agrega un elemento al final del array*/
-    console.log(elemento_siguiente.toFixed(4)); /* toFixed() es un método para aproximar un número con cierta cantidad de decimales*/
+    array_inicial.push(parseFloat(elemento_siguiente.toFixed(3))); /* push() es un método de los array que agrega un elemento al final del array*/
+     /* toFixed() es un método para aproximar un número con cierta cantidad de decimales*/
 }
+
+console.log('Los nuevo números son:', array_inicial)
 
